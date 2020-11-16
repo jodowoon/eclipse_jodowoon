b3a81203d2cfa5fa8ad9c6f65972c53fdc89f4fa
student_jodowoon <student_dowoon@naver.com>
2020-11-16 09:25:19 +0900

    add sophomore

--- hello_world/src/hello_world/printpush.java
+++ hello_world/src/hello_world/printpush.java
@@ -3,8 +3,8 @@ package hello_world;
 public class printpush {
 
 	public static void main(String[] args) {
-		System.out.println("add java class file push");
-
+		System.out.println("sophomore");
+		
 	}
 
 }
